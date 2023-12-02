b33b33bb3c5027afbcd4f920754a432e69014b72
ross-brown <rbrown_98@yahoo.com>
2023-12-01 23:48:51 -0600

    finish lesson 9

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -4,7 +4,10 @@ app.component('product-details', {
       type: Array,
       required: true
     }
-  }, template:
-  /*html*/
-  ``
-})
+  },
+  template:
+    /*html*/
+    `<ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>`
+});
